f2e017f9dd0e8a9c9b199acbada41c71ca61a3fa
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 13:06:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
+streamlit.write('The user entered ', add_my_fruit)
+
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
