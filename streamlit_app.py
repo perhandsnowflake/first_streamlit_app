4023ca9ca1506204054c944220bc4821701d40ca
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 16:44:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,6 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list ( )
     streamlit.dataframe(my_data_rows)
     
-
-
 #add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
 #streamlit.write('Thanks for adding ', add_my_fruit)
 
