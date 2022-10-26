b2717cc02afb4d9707e11cb8bce6b6e44bce067a
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 16:21:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,22 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+
 streamlit.header("Fruityvice Fruit Advice!")
+
+def get_fruityvice_response (this_fruit_choice ):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice )
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_response(fruit_choice)
+      streamlit.dataframe(back_from_function)
   
 except URLError as e:
   streamlit.error();
