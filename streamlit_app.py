2e9c3d4a29cc0f985a857ccb88d351cb4151c8fa
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 16:46:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,10 +73,10 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 # Add button
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list ( )
-    streamlit.dataframe(my_data_rows)
+#if streamlit.button('Get Fruit Load List'):
+ #   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  #  my_data_rows = get_fruit_load_list ( )
+  #  streamlit.dataframe(my_data_rows)
     
 #add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
 #streamlit.write('Thanks for adding ', add_my_fruit)
