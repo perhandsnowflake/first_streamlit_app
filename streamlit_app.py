db4c8b031ddc32433ae9efc07530a8d0d4b986c8
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 16:50:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-def get_fruityvice_response (this_fruit_choice ):
+def get_fruityvice_data (this_fruit_choice ):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice )
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -37,7 +37,7 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      back_from_function = get_fruityvice_response(fruit_choice)
+      back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
   
 except URLError as e:
@@ -73,10 +73,10 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 # Add button
-#if streamlit.button('Get Fruit Load List'):
- #   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  #  my_data_rows = get_fruit_load_list ( )
-  #  streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list ( )
+    streamlit.dataframe(my_data_rows)
     
 #add_my_fruit = streamlit.text_input('What fruit would you like information about?','')
 #streamlit.write('Thanks for adding ', add_my_fruit)
