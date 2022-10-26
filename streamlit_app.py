7bdf08dc0d218cb557716952fe4ce1771e8b0c1c
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-26 10:40:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-imoport snowflake.connector
+#imoport snowflake.connector
 
 
 
