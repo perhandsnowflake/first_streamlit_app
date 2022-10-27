8d5b10bbc82173b25368a5c9b8078eebdb0353ad
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-27 09:51:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
 if streamlit.button('Add a Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake (add_my_fruit)
-    streamlit.text(back_from_function)
+    return streamlit.text(back_from_function)
     
     
  streamlit.write('Thanks for adding ', add_my_fruit)
