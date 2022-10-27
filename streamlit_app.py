e35ac63120d3b226ae7d45049fd575e95b7041a3
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-27 09:53:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,7 +92,7 @@ if streamlit.button('Add a Fruit to the list'):
     return streamlit.text(back_from_function)
     
     
- streamlit.write('Thanks for adding ', add_my_fruit)
+ #streamlit.write('Thanks for adding ', add_my_fruit)
 streamlit.stop()
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
