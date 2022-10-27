87ce470d929df929a834c444276328ea10ff8b97
Perhand Snowflake <116087436+perhandsnowflake@users.noreply.github.com>
2022-10-27 10:04:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ if streamlit.button('Add a Fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake (add_my_fruit)
     streamlit.text(back_from_function)
-    streamlit.write('Thanks for adding ', add_my_fruit)
+    #streamlit.write('Thanks for adding ', add_my_fruit)
     
 streamlit.stop()
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
